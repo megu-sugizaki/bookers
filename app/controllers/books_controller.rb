5e02bb0662230eb224e854ee7c2c927dd8578ded
EC2 Default User <ec2-user@ip-172-31-9-47.ap-northeast-1.compute.internal>
2023-09-09 05:41:39 +0000

    フラッシュメッセージ表示エラー解消

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,17 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      @book = Book.new
+      @books = Book.all
+      flash.now[:notice] = "2 errors prohibited this book from being saved."
+      render :index
+    end
+    
   end
 
   def show
@@ -24,8 +33,14 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id) #showへのリダイレクト
+    
+    if book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(book.id) #showへのリダイレクト
+    else
+     render :edit
+    end
+    
   end 
   
   def destroy
